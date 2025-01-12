a30a3abb00bcb80bd47873a2157eba0c67daaa26
YousofLHC <yousof.ghalenoei2017@gmail.com>
2025-01-12 17:00:49 +0300

    Expose DecisionBoundaryVisualizer in the module.

--- svddlib/__init__.py
+++ svddlib/__init__.py
@@ -1,7 +1,7 @@
 # svddlib/__init__.py
 
 from .svdd import SVDD
-#from .visualizer import DecisionBoundaryVisualizer 
+from .visualizer import DecisionBoundaryVisualizer 
 # #--> from svddlib import SVDD, DecisionBoundaryVisualizer instead of 
 # --> from svddlib.svdd import SVDD,  from svddlib.visualizer import DecisionBoundaryVisualizer
 
