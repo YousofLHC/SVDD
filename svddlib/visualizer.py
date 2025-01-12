670c9e8427ff496d0b24770f4c5b5b3fb90bacb8
YousofLHC <yousof.ghalenoei2017@gmail.com>
2025-01-12 17:00:11 +0300

    Add plot_decision_boundary method for DecisionBoundaryVisualizer

--- svddlib/visualizer.py
+++ svddlib/visualizer.py
@@ -41,3 +41,37 @@ class DecisionBoundaryVisualizer:
         title : str, optional
             Title of plot.
         """
+        if X.shape[1]!=2:
+            raise ValueError("Visualization is only supported for 2D data.")
+        
+        # Generate grid
+        x_min, x_max = X[:, 0].min()-1, X[:, 0].max()+1
+        y_min, y_max = X[:, 1].min()-1, X[:, 1].max()+1
+        xx   , yy    = np.meshgrid(
+            np.linspace(x_min, x_max, self.resolution),
+            np.linspace(y_min, y_max, self.resolution)
+        )
+        grid = np.c_[xx.ravel(), yy.ravel()]
+
+        # Calculate decision scores
+        distances = self.svdd_model.decision_function(grid)
+        decision_scores = distances.reshape(xx.shape)
+
+        # Plot decision boundary
+        plt.figure(figsize=(10,8))
+        plt.contour(xx, yy, decision_scores, cmap=self.cmap, alpha=0.8)
+        plt.colorbar(label='Decision Score')
+
+        # Plot data points
+        if y is None:
+            plt.scatter(X[:,0], X[:,1], c=y, cmap=self.cmap, edgecolors='k')
+        else:
+            plt.scatter(X[:,0], X[:,1], color='black', edgecolors='k')
+        
+        # Add title and labels
+        if title:
+            plt.title(title)
+        plt.xlabel('Feature 1')
+        plt.ylabel('Feature 2')
+        plt.grid()
+        plt.show()
