4cd77c5a51d2baafcaed507b956b92431a59e0c0
YousofLHC <yousof.ghalenoei2017@gmail.com>
2025-01-12 16:39:02 +0300

    Add initial structure for visualizer module.

---
+++ svddlib/visualizer.py
@@ -0,0 +1,8 @@
+# svddlib/visualizer.py
+
+"""
+Visualizer for SVDD Decision Boundary.
+
+This module contains the `DecisionBoundaryVisualizer` class
+for visualizing the decision boundary of an SVDD model.
+"""
